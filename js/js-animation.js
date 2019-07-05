ae22be18ff6d222166351ef9178a2ea8585f66e4
stred13 <34312900+stred13@users.noreply.github.com>
2019-07-05 21:23:01 +0700

    update index.html

--- js/js-animation.js
+++ js/js-animation.js
@@ -1,6 +1,33 @@
-$(document).ready(function() {
-  $('#media').carousel({
+$(document).ready(function () {
+  $('#ex1').slider({
+  });
+
+  $('#ex2').slider({
+  });
+  
+  
+   $('#media').carousel({
     pause: true,
     interval: false,
   });
+
+  $(".owl-carousel").owlCarousel({
+    loop:true,
+    responsive:{
+      0:{
+        items:1
+      },
+      480:{
+        items:2
+      },
+      768:{
+        items:4
+      },
+    }
+  });
+
+
 });
+
+
+
