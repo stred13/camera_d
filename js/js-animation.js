bb3cae04ec95397cdbd9fd136778ff1883af466a
stred13 <34312900+stred13@users.noreply.github.com>
2019-07-25 18:05:11 +0700

    add img

--- js/js-animation.js
+++ js/js-animation.js
@@ -4,29 +4,33 @@ $(document).ready(function () {
 
   $('#ex2').slider({
   });
-  
-  
-   $('#media').carousel({
+
+  $('#media').carousel({
     pause: true,
     interval: false,
   });
 
   $(".owl-carousel").owlCarousel({
-    loop:true,
-    responsive:{
-      0:{
-        items:1
+    loop: true,
+    responsive: {
+      0: {
+        items: 1
       },
-      480:{
-        items:2
+      480: {
+        items: 2
       },
-      768:{
-        items:4
+      768: {
+        items: 4
       },
     }
   });
 
-
+  $('a').click(function(){
+    var producer = $(this).text();
+    $.get("",{type: s},function(response){
+      alert($(this).text());
+    });
+  });
 });
 
 
